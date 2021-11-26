9bf20e32068197c020ee72a2e76453909d61b2de
Sebastian Peschke <magick930@googlemail.com>
2021-11-26 01:16:03 +0100

    parser fixed contents are set up

--- app/src/main/java/com/example/unistundenplan/SettingsFragment.java
+++ app/src/main/java/com/example/unistundenplan/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.example.unistundenplan;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -16,6 +17,7 @@ import com.example.unistundenplan.data.CourseData;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,12 +26,11 @@ import java.util.List;
  */
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private ArrayList<Course> coursesArrayList = new ArrayList<>();
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     private Spinner spinner;
@@ -39,15 +40,6 @@ public class SettingsFragment extends Fragment {
 
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SettingsFragment newInstance(String param1, String param2) {
         SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
@@ -74,6 +66,12 @@ public class SettingsFragment extends Fragment {
 
     }
 
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loadCourses();
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -84,23 +82,22 @@ public class SettingsFragment extends Fragment {
     }
 
     public void loadCourses() {
-        final Spinner spinner = getView().findViewById(R.id.courses);
-        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-        List<String> categories = new ArrayList<String>();
-        categories.add("Item 1");
-        categories.add("Item 2");
-        categories.add("Item 3");
-        categories.add("Item 4");
-        categories.add("Item 5");
-        categories.add("Item 6");
+        CourseData.getCourses(courses -> {
+            if (courses.isPresent()) {
+                Spinner courseSpinner = getView().findViewById(R.id.courses);
+                String[] strings = courses.get().stream()
+                        .map(Course::toString)
+                        .collect(Collectors.toList()).toArray(new String[courses.get().size()]);
 
-        // ArrayAdapter<CharSequence> dataAdapter;
-        // dataAdapter = ArrayAdapter.createFromResource(
-        //         getActivity().getBaseContext(),
-//                categories,
-//                android.R.layout.simple_spinner_item);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity().getBaseContext(), android.R.layout.simple_spinner_dropdown_item, categories);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(arrayAdapter);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, strings);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                courseSpinner.setAdapter(adapter);
+
+                this.coursesArrayList = courses.get();
+            } else {
+                //showErrorAlert();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/unistundenplan/data/CourseData.java
+++ app/src/main/java/com/example/unistundenplan/data/CourseData.java
@@ -1,14 +1,30 @@
 package com.example.unistundenplan.data;
 
+import android.os.Looper;
+import android.os.Handler;
+
 import org.jsoup.Jsoup;
 
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class CourseData {
-    final ArrayList<String> coursesOut = new ArrayList<>();
-    final ArrayList<Course> toCourses = new ArrayList<>();
+    private static final ArrayList<String> coursesOut = new ArrayList<>();
+    private static final ArrayList<Course> toCourses = new ArrayList<>();
 
-    public ArrayList <String> parseCourses(String url){
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final ExecutorService exe = Executors.newFixedThreadPool(2);
+    private static final Handler handler = new Handler(Looper.getMainLooper());
+
+    public static ArrayList <String> parseCourses(String url){
 
         try{
             org.jsoup.nodes.Document doc = Jsoup.parse(url);
@@ -16,20 +32,33 @@ public class CourseData {
 
             for(org.jsoup.nodes.Element element : select.children()){
                 if(!element.text().contains("wählen")) {
-                    coursesOut.add(element.text());
-                    Course course = new Course(element.id(), element.text());
-                    toCourses.add(course);
+                    toCourses.add(new Course(element.val(), element.text()));
                 }
             }
 
         }catch (Exception e){
-
         }
         return coursesOut;
     }
 
-    public static void getCourses(){
+    public static void getCourses(Consumer<Optional<ArrayList<Course>>> coursesCallback) {
+        if (coursesOut.isEmpty()) {
+            Request request = new Request.Builder()
+                    .url("https://hof-university.de/studierende/info-service/stundenplaene/")
+                    .build();
 
+            exe.submit(() -> {
+                try (Response response = client.newCall(request).execute()) {
+                    parseCourses(response.body().string());
+                    handler.post(() -> coursesCallback.accept(Optional.of(toCourses)));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    handler.post(() -> coursesCallback.accept(Optional.empty()));
+                }
+            });
+        } else {
+            coursesCallback.accept(Optional.of(toCourses));
+        }
     }
 
 }
